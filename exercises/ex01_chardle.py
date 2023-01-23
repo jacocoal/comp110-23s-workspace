84f2ff10ce54e9917f01ef4b35984669de587870
Jacob Coale <jcoale@unc.edu>
2023-01-22 23:13:43 -0500

    Progress on ex.01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -12,25 +12,25 @@ if(len(character)!=1):
 chrcount: int = 0
 print("Searching for " + character ,"in " + word)
 if(word[0] == character):
-    print(character + " found at index 1")
+    print(character + " found at index 0")
     chrcount = chrcount+1
 if(word[1]==character):
-    print(character + " found at index 2")
+    print(character + " found at index 1")
     chrcount = chrcount+1
 if(word[2]==character):
-    print(character + " found at index 3")
+    print(character + " found at index 2")
     chrcount = chrcount+1
 if(word[3]==character):
-    print(character + " found at index 4")
+    print(character + " found at index 3")
     chrcount = chrcount+1
 if(word[4]==character):
-    print(character + " found at index 5")
+    print(character + " found at index 4")
     chrcount = chrcount+1
 if(chrcount == 0):
     print("No instances of " + character + " found in "+ word)
 else:(chrcount != 0)
 if(chrcount == 1):
-    print(chrcount, " intstance of " + character + " found in " + word)
+    print( chrcount," intstance of " + character + " found in " + word)
 else:(chrcount != 1)
 if(chrcount>1):
-    print(chrcount, " instances of " + character + " found in " + word)
+    print( chrcount," instances of " + character + " found in " + word)
