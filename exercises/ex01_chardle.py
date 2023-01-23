0a8f88cedb7258eb8a832c1c72ae9ac72645e14c
Jacob Coale <jcoale@unc.edu>
2023-01-22 23:00:26 -0500

    Progress on Exercise 1

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,36 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730481986"
+word: str = input("Enter a 5-character word: ")
+if(len(word)!=5):
+    print ("Error: Word must contain 5 characters")
+    exit()
+character: chr = input("Enter a single character: ")
+if(len(character)!=1):
+    print("Error: Character must be a single character.")
+    exit()
+chrcount: int = 0
+print("Searching for " + character ,"in " + word)
+if(word[0] == character):
+    print(character + " found at index 1")
+    chrcount = chrcount+1
+if(word[1]==character):
+    print(character + " found at index 2")
+    chrcount = chrcount+1
+if(word[2]==character):
+    print(character + " found at index 3")
+    chrcount = chrcount+1
+if(word[3]==character):
+    print(character + " found at index 4")
+    chrcount = chrcount+1
+if(word[4]==character):
+    print(character + " found at index 5")
+    chrcount = chrcount+1
+if(chrcount == 0):
+    print("No instances of " + character + " found in "+ word)
+else:(chrcount != 0)
+if(chrcount == 1):
+    print(chrcount, " intstance of " + character + " found in " + word)
+else:(chrcount != 1)
+if(chrcount>1):
+    print(chrcount, " instances of " + character + " found in " + word)
