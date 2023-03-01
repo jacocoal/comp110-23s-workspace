3bfaee85e43ac00c577cec565e2a7eef3054d2f8
Jacob Coale <jcoale@unc.edu>
2023-02-28 22:16:44 -0500

    ex_04 2nd try

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,11 +1,13 @@
-"""Recreating the commonly used python functions all, max, and is_equal"""
+"""Recreating the commonly used python functions all, max, and is_equal."""
 
 __author__ = "730481986"
-def all(int_list:list[int],single_int = int) -> bool:
-    """Returns true if the single integer is found within the list and false if it is not"""
+
+
+def all(int_list: list[int], single_int:int) -> bool:
+    """Returns true if the single integer is found within the list and false if it is not."""
     list_length: int = len(int_list)
     idx: int = 0
-    if(list_length == 0):
+    if (list_length == 0):
         return False
     while idx < list_length:
         if (single_int == int_list[idx]):
@@ -14,8 +16,9 @@ def all(int_list:list[int],single_int = int) -> bool:
             return False
     return True
 
+
 def max(int_list: list[int]) -> int:
-    """Returns the largest number in a list"""
+    """Returns the largest number in a list."""
     if len(int_list) == 0:
         raise ValueError("max() arg is an empty List")
     list_length: int = len(int_list)
@@ -29,8 +32,9 @@ def max(int_list: list[int]) -> int:
             idx += 1
     return current_max
 
+
 def is_equal(int_list1: list[int], int_list2: list[int]) -> bool:
-    """Returns if two lists are exactly equal"""
+    """Returns if two lists are exactly equal."""
     if len(int_list1) != len(int_list2):
         return False
     idx: int = 0
