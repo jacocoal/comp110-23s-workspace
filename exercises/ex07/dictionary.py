49f7db975bd9e58179aa42b0b6d67c7d0d591e03
Jacob Coale <jcoale@unc.edu>
2023-03-30 00:49:35 -0400

    first attempt

---
+++ exercises/ex07/dictionary.py
@@ -0,0 +1,68 @@
+"""Practicing dicitonary functions."""
+
+__author__ = "730481986"
+
+
+def invert(dictionary: dict[str,str]) -> dict[str,str]:
+    """Flips values of a dictionary so that keys become values and values become keys."""
+    result: dict[str,str] = {}
+    new_key_list: list[str] = []
+    old_key_list: list[str] = []
+    x: int = 0
+    check_idx: int = 0
+    for key in dictionary:
+        new_key_list.append(dictionary[key])
+        old_key_list.append(key)
+    while x < len(new_key_list):
+        while check_idx < len(new_key_list):
+            if x != check_idx:
+                if new_key_list[x] == new_key_list[check_idx]:
+                    raise KeyError(f"Key {new_key_list[x]} was found more than once. ")
+            check_idx += 1
+        result[new_key_list[x]] = old_key_list[x]
+        x += 1
+    return result
+
+
+def favorite_color(names_and_colors: dict[str,str]) -> str:
+    """Returns the most common favorite color."""
+    list_of_fav_colors: list[str] = []
+    idx: int = 0
+    check_idx: int = 0
+    tally: dict[str,int] = {}
+    best: int = 0
+    best_color: str = ""
+    for key in names_and_colors:
+        list_of_fav_colors.append(names_and_colors[key])
+    while idx < len(list_of_fav_colors):
+        if list_of_fav_colors[idx] not in tally:
+            tally[list_of_fav_colors[idx]] = 1
+            check_idx = 0
+            while check_idx < len(list_of_fav_colors):
+                if list_of_fav_colors[idx] == list_of_fav_colors[check_idx]: 
+                    tally[list_of_fav_colors[idx]] += 1 
+                check_idx += 1
+        idx += 1
+    for key in tally:
+        if tally[key] > best:
+            best = tally[key]
+            best_color = key
+    return best_color
+
+
+def count(list1: list[str]) -> dict[str,int]:
+    """Returns how many times a number is found in a list."""
+    new_dict: dict[str,int] = {}
+    for item in list1:
+        if item in new_dict:
+            new_dict[item] += 1
+        else:
+            new_dict[item] = 1
+    return new_dict
+
+                    
+
+        
+
+
+        
---
+++ exercises/ex07/dictionary_test.py
@@ -0,0 +1,60 @@
+"""Unit tests for the dictionary file."""
+
+__author__ = "730481986"
+
+
+from exercises.ex07.dictionary import invert, favorite_color, count
+
+
+def test_regular_invert() -> None:
+    """Tests the invert funtion to make sure a multitude of entries returns the correct result."""
+    test_many_dict: dict[str,str] = {'hair': 'red', 'shirt': 'blue', 'pants': 'brown'}
+    assert invert(test_many_dict) == {'red': 'hair', 'blue': 'shirt', 'brown': 'pants'}
+
+
+def test_many_invert() -> None:
+    """Tests the invert function to make sure more than 5 entries returns the correct amount."""
+    test_integers_dict: dict[str,str] = {"cold": "steel", "big": "chungus", "Marty": "McFly", "Johnny": "Knoxville", "Bradley": "Nowell"}
+    assert invert(test_integers_dict) == {"steel": "cold", "chungus": "big", "McFly": "Marty", "Knoxville": "Johnny", "Nowell": "Bradley"}
+
+
+def test_empty_invert() -> None:
+    """Tests the invert function to make sure an empty dictionary returns and empty list."""
+    test_empty_dict: dict[str,str] = {}
+    assert invert(test_empty_dict) == {}
+
+
+def test_favorite_color() -> None:
+    """Tests to make sure the correct favorite color is returned in a list."""
+    colors: dict[str,str] = {"Marc": "yellow", "Ezri": "blue", "Kris": "blue"}
+    assert favorite_color(colors) == "blue"
+
+
+def test_more_than_two() -> None:
+    """Tests to make sure that the color that is listed on the dictionary first is returned when there is a tie for favorite color."""
+    colors: dict[str,str] = {"Marc": "yellow", "Ezri": "blue"}
+    assert favorite_color(colors) == "yellow"
+
+
+def test_empty_colors() -> None:
+    """Tests to return nothing when the dictionary is empty."""
+    colors: dict[str, str] = {}
+    assert favorite_color(colors) == ""
+
+
+def test_regular() -> None:
+    """Tests to ensure count works properly given regular inputs."""
+    list1: list[str] = ["dog", "cat", "dog", "bird"]
+    assert count(list1) == {"dog": 2, "cat": 1, "bird": 1}
+
+
+def test_large() -> None:
+    """Tests to ensure count works properly given more than 5 inputs."""
+    list1: list[str] = ["dog", "cat", "dog", "bird", "llama", "cat", "dog"]
+    assert count(list1) == {"dog": 3, "cat": 2, "bird": 1, "llama": 1}
+
+
+def test_empty_count() -> None:
+    """Tests to return nothing when the list is empty."""
+    list1: list[str] = []
+    assert count(list1) == {}
